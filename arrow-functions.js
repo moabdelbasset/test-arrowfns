f333a329451ed8e79604d351ecb9118d0360f3aa
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-09-28 18:11:58 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
-
+//function addTwoNumbers(a, b) {
+    // Code block
+ //   return a + b;
+//}
+//let sum = addTwoNumbers(3, 5);
+//console.log(sum);
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => a + b;
+let sum2 = addTwoNumbers2(2, 7);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMuliplelines = () => (
+    `
+    Test line 1
+    test line 2
+    `
+)
